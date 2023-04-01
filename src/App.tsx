69a9e0b24db1b8222d45b1ab8a41750846fc49fa
Roland <roland.paire@gmx.fr>
2023-04-01 11:03:22 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const title = 'React';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Road to {title}</h1>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" />
+      {[1, 2, 3].map((item) => (
+        <ul>{item}</ul>
+      ))}
     </div>
   );
 }
