c833eabf0f2e2de81d0daeedc565ed21e57f5f2f
Roland <roland.paire@gmx.fr>
2023-04-01 16:33:52 +0200

    React Impossible states

--- src/App.tsx
+++ src/App.tsx
@@ -11,44 +11,133 @@ type Story = {
 
 type Stories = Story[];
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = (): Promise<{ data: { stories: Stories } }> =>
+  new Promise((resolve) => setTimeout(() => resolve({ data: { stories: initialStories } }), 2000));
+
+type StoriesState = {
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+type StoriesSetAction = {
+  type: 'SET_STORIES';
+  payload: Stories;
+};
+
+type StoriesRemoveAction = {
+  type: 'REMOVE_STORY';
+  payload: Story;
+};
+
+type StoriesFetchInit = {
+  type: 'STORIES_FETCH_INIT';
+};
+type StoriesFetchFailure = {
+  type: 'STORIES_FETCH_FAILURE';
+};
+
+type StoriesFetchSuccess = {
+  type: 'STORIES_FETCH_SUCCESS';
+  payload: Stories;
+};
+
+type StoriesAction =
+  | StoriesSetAction
+  | StoriesRemoveAction
+  | StoriesFetchInit
+  | StoriesFetchFailure
+  | StoriesFetchSuccess;
+const storiesReducer = (state: StoriesState, action: StoriesAction) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter((story) => action.payload.objectID !== story.objectID),
+      };
+    default:
+      throw new Error();
+  }
+};
+const useStorageState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
-  const useStorageState = (
-    key: string,
-    initialState: string
-  ): [string, (newValue: string) => void] => {
-    const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
-    React.useEffect(() => {
-      localStorage.setItem(key, value);
-    }, [value, key]);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
 
-    return [value, setValue];
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    getAsyncStories()
+      .then((result) => {
+        dispatchStories({ type: 'STORIES_FETCH_SUCCESS', payload: result.data.stories });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, []);
+
+  const handleRemoveStory = (item: Story) => {
+    dispatchStories({ type: 'REMOVE_STORY', payload: item });
   };
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  const searchStories = stories.filter((story) =>
+  const searchedStories = stories.data.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -56,71 +145,32 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      {/*<Search onSearch={handleSearch} searchTerm={searchTerm} />*/}
-      <InputWithLabel id="search" value={searchTerm} onInputChange={handleSearch} isFocused>
-        Search:
+      <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={handleSearch}>
+        <strong>Search:</strong>
       </InputWithLabel>
 
       <hr />
 
-      <List list={searchStories} />
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
-
-type SearchProps = {
-  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  searchTerm: string;
-};
-// eslint-disable-next-line
-const Search: React.FC<SearchProps> = (props) => {
-  return (
-    <>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} defaultValue={props.searchTerm} />
-
-      <p>
-        Searching for <strong>{props.searchTerm}</strong>.
-      </p>
-    </>
-  );
-};
-
-type ListProps = {
-  list: Stories;
-};
-
-const List: React.FC<ListProps> = (props) => (
-  <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
-    ))}
-  </ul>
-);
-
-type ItemProps = {
-  item: Story;
-};
-
-const Item: React.FC<ItemProps> = (props) => (
-  <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
-);
 
 type InputWithLabelProps = {
   id: string;
   value: string;
   type?: string;
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  children: React.ReactNode;
   isFocused: boolean;
+  children: React.ReactNode;
 };
+
 const InputWithLabel: React.FC<InputWithLabelProps> = ({
   id,
   value,
@@ -130,20 +180,54 @@ const InputWithLabel: React.FC<InputWithLabelProps> = ({
   children,
 }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
+
   React.useEffect(() => {
     if (isFocused && inputRef.current) {
       inputRef.current.focus();
     }
   }, [isFocused]);
+
   return (
     <>
-      <label htmlFor={id}>{children} </label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
-      <input id={id} type={type} onChange={onInputChange} value={value} ref={inputRef} />
-      <p>
-        Searching for <strong>{value}</strong>.
-      </p>
+      <input ref={inputRef} id={id} type={type} value={value} onChange={onInputChange} />
     </>
   );
 };
+
+type ListProps = {
+  list: Stories;
+  onRemoveItem: (item: Story) => void;
+};
+
+const List: React.FC<ListProps> = ({ list, onRemoveItem }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ))}
+  </ul>
+);
+
+type ItemProps = {
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+};
+
+const Item: React.FC<ItemProps> = ({ item, onRemoveItem }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
+
 export default App;
